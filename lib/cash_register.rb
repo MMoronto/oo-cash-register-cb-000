d501dd4d376b9bccec5c176808f42e11c5f0c889
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-03-14 01:21:59 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,11 @@
-
+class CashRegister
+  attr_accessor :item, :discount, :total, :last_transaction
+  
+  @@all = []
+  
+  def initialize(total = 0)
+    @total = total
+    add_items = add_items
+  end 
+  
+end 
